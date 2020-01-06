28252a1a539ecc990feec2139b3baadd08c79029
nickie kane <kameo712@gmail.com>
2020-01-06 01:53:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@ require 'pry'
 def reverse_each_word(string)
   array = string.split(" ")
   n_array = []
-  n_array = array.each{|n| n.reverse}
-  string.each { |n| puts "#{n}" }
+  n_array = array.collect{|n| n.reverse}
   n_array.join(" ")
   # binding.pry
 end
